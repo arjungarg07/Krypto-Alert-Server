4288946aeaf3fa9ac10d6b701ddae502c103b30a
arjungarg07 <arjun.garg.cse01@gmail.com>
2021-10-05 22:48:33 +0530

    feat: setup mongoDb connection

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
 require('dotenv').config();
-const express = require("express");
+const express = require('express');
+const mongoose = require('mongoose');
 
-const PORT = process.env.port || 8000
+const updateCurrencyJob = require('./jobs/updateCurrency');
+const userRoutes = require('./routes/user');
+
+const PORT = process.env.PORT || 8000;
+const URL = process.env.MONGODB_URI;
+
+express.urlencoded({extended: true});
+
 const app = express();
 
-app.listen(PORT,()=>{
-    console.log(`Server listening on ${PORT}`);
+// Connect to mongoDb
+mongoose
+  .connect(URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('Connected to database!');
+  })
+  .catch((err) => {
+    console.log('Cannot connect to database:', err);
+  });
+
+updateCurrencyJob.start();
+
+app.use('/api/v1',userRoutes);
+
+app.listen(PORT,() => {
+  console.log(`Server listening on ${PORT}`);
 });
