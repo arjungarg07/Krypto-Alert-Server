32997cbae8cecdc7b01638a0f84cbf86486a9e18
arjungarg07 <arjun.garg.cse01@gmail.com>
2021-10-12 00:16:31 +0530

    feat: implement jwt verification service

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,15 +1,17 @@
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
 
 const User = require('../models/UserModel');
+const emailController = require('./emailController');
 
 const saltRounds = 10;
 
 class authController {
-  generateJwtToken(credentials) {
+  generateJwtToken(credentials, expiry) {
     const secretKey = process.env.PRIVATEKEY; 
     return new Promise((resolve,reject) => {
-      jwt.sign(credentials,secretKey, { expiresIn: 60 * 15 }, (err, token) => {
+      jwt.sign(credentials,secretKey, { expiresIn: expiry }, (err, token) => {
         if (err) reject(err);
         resolve(token);
       });
@@ -34,10 +36,19 @@ class authController {
     if (typeof bearerHeader !== 'undefined') {
       const bearerToken = bearerHeader.split(' ')[1];
       // set the token
-      res.token = bearerToken;
-      next();
+      const secretKey = process.env.PRIVATEKEY; 
+      jwt.verify(bearerToken,secretKey,(err,decoded) => {
+        if (err) {
+          console.log(err);
+          return next();
+        }
+        const { userId, username } = decoded;
+        console.log('verify',decoded);
+        req.authData = { userId, username };
+        next();
+      });
     } else {
-      res.status(403).send({status: 0, msg: 'Forbidden access'});
+      res.status(403).json({status: 0, msg: 'Forbidden access'});
     }
   }
 
@@ -45,17 +56,22 @@ class authController {
     try {
       const {firstName,lastName,username,password,emailId} = req.body;
       const pwdHash = await this.generateHash(password);
-      const userData = {firstName,lastName,username,pwdHash,emailId};
+      const userData = new User({ _id: new mongoose.Types.ObjectId(), firstName,lastName,username,pwdHash,emailId});
       await User.create(userData);
+      const URL = await this.generateVerifyURL({username, emailId});
+      await emailController.sendVerification(URL,emailId);
       res.send({
         status: 1,
         msg: 'User registered successfully'
       });
     } catch (err) {
-      let msg;
-      if (err.code === 11000) msg = 'Username or EmailId already exists';
+      if (err.code === 11000) {
+        res.status(400).json({status: 0, msg: 'Username or emailId already exists'});
+        // console.log(err);
+        return;
+      }
       console.log(err);
-      res.send({status: 0,msg});
+      res.status(500).json({status: 0,msg: 'Internal Server Error'});
     }
   }
 
@@ -64,19 +80,40 @@ class authController {
       const {username,password} = req.body;
 
       const userData = await User.findOne({username});
-      if (!userData) res.send({status: 1, msg: 'Username doesn\'t exist'});
+      if (!userData) res.status(400).json({status: 1, msg: 'Username doesn\'t exist'});
 
-      const { emailId, pwdHash } = userData; 
+      const { emailId, pwdHash, _id } = userData; 
 
       const match = await bcrypt.compare(password, pwdHash);
-      if (!match) res.send({status: 1, msg: 'Entered password is wrong'});
+      if (!match) res.status(400).json({status: 1, msg: 'Unable to log in. Please ensure that your username and password are correct'});
       
-      const credentials = { username, emailId};
-      const token = await this.generateJwtToken(credentials);
+      const credentials = { username, emailId, userId: _id};
+      const token = await this.generateJwtToken(credentials,60 * 60);
 
-      res.send({ success: 1,token });
+      res.send({ success: 1,token});
     } catch (err) {
       console.log(err);
+      res.status(500).json({status: 0, msg: 'Something went wrong'});
+    }
+  }
+
+  async generateVerifyURL(creds) {
+    const token = await this.generateJwtToken(creds, 60 * 60 * 24 * 2);
+    return `localhost:8000/api/v1/signup/validate/?token=${token}`;
+  }
+
+  async verifyUser(req,res) {
+    try {
+      const { token } = req.query;
+      const secretKey = process.env.PRIVATEKEY;
+      const decoded = await jwt.verify(token,secretKey);
+      const { username } = decoded; // username, emailId
+      this.username = username;
+      await User.findOneAndUpdate({username},{isVerified: true});
+      res.json({status: 1, msg: 'Successfully verified User'});
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({status: 0, msg: 'Something went wrong'});
     }
   }
 }
--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,14 @@
 const router = require('express').Router();
+
+const alertController = require('../controllers/alertController');
 const authController = require('../controllers/authController');
+const userController = require('../controllers/userController');
 
 router.post('/signup',authController.signUpUser.bind(authController));
 router.get('/login',authController.login.bind(authController));
+router.get('/signup/validate/',authController.verifyUser.bind(authController));
+
+router.get('/:username',authController.verifyToken,userController.data);
+router.get('/:username/alert',authController.verifyToken,alertController.createAlert.bind(alertController));
 
 module.exports = router;
