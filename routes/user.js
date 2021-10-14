8260f5307e34641adbcf4d8b296b444ebcbc7d2e
arjungarg07 <arjun.garg.cse01@gmail.com>
2021-10-15 00:44:54 +0530

    feat: add get top currency route

--- routes/user.js
+++ routes/user.js
@@ -2,13 +2,18 @@ const router = require('express').Router();
 
 const alertController = require('../controllers/alertController');
 const authController = require('../controllers/authController');
+const currencyController = require('../controllers/currencyController');
 const userController = require('../controllers/userController');
 
 router.post('/signup',authController.signUpUser.bind(authController));
 router.get('/login',authController.login.bind(authController));
 router.get('/signup/validate/',authController.verifyUser.bind(authController));
 
+router.get('/data/all',currencyController.getAll);
+router.get('/data/top',currencyController.getTop);
+
 router.get('/:username',authController.verifyToken,userController.data);
-router.get('/:username/alert',authController.verifyToken,alertController.createAlert.bind(alertController));
+router.post('/:username/alert',authController.verifyToken,alertController.createAlert.bind(alertController));
+router.put('/:username/alert',authController.verifyToken,alertController.deleteAlert.bind(alertController));
 
 module.exports = router;
