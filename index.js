63a9b9f678996de0617c12a32c46b0f2a007d26b
arjungarg07 <arjun.garg.cse01@gmail.com>
2021-10-23 04:50:50 +0530

    feat: add cookie parser

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const compression = require('compression');
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
 const updateCurrencyJob = require('./jobs/updateCurrency');
 const userRoutes = require('./routes/user');
 
@@ -10,9 +11,12 @@ const PORT = process.env.PORT || 8000;
 const URL = process.env.MONGODB_URI;
 
 const app = express();
+app.use(cookieParser());
 app.use(express.urlencoded({extended: true}));
-app.use(cors());
+app.use(express.json({extended: true, type: 'application/json'}));
+app.use(cors({credentials: true, origin: true}));
 // decrease the size of the req body to incr. performance
+
 app.use(compression());
 
 // Connect to mongoDb
@@ -27,36 +31,7 @@ mongoose
   .catch((err) => {
     console.log('Cannot connect to database:', err);
   });
-// const obj = {};
-// Iterate icon.json and download all the icons in the folder
-// const icons = require('./icon.json');
-// Iterate on icons object and download all the icons
-// for (const [key,value] of Object.entries(icons)) {
-//   const iconName = key;
-//   const iconUrl = value;
-//   const iconPath = `./images/${iconName}.png`;
-//   // Download the icon
-//   fetch(iconUrl)
-//     .then(res => res.buffer())
-//     .then(buffer => fs.writeFileSync(iconPath, buffer));
-// };
 
-// const iconsArray = 
-// fetch('https://bitbns.com/jugApi/globalParams.json?rdm=206004948452267')
-//   .then(res => res.json())
-//   .then(result => {
-//     console.log(result[0].data.coins);
-//     const coinData = result[0].data.coins;
-//     // Iterate on coinData Object and pick keys and values
-//     // push new object to obj
-//     for (const [key, value] of Object.entries(coinData)) {
-//       obj[key] = value.coinIcon;
-//     }
-//     console.log(obj);
-//   }).then(() => {
-//     // save obj to json file
-//     fs.writeFileSync('icon.json', JSON.stringify(obj));
-//   });
 updateCurrencyJob.start();
 
 app.use('/api/v1',userRoutes);
