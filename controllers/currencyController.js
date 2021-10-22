db272f5e60bd602d9358152bf5094c66bf150227
arjungarg07 <arjun.garg.cse01@gmail.com>
2021-10-23 04:52:41 +0530

    feat: switch to coingecko api

--- controllers/currencyController.js
+++ controllers/currencyController.js
@@ -1,77 +1,91 @@
-const fetch = require('isomorphic-fetch');
-const Currency = require('../models/CurrencyModel');
-const coins = require('../acronym.json');
+const fetch = require("isomorphic-fetch");
+const Currency = require("../models/CurrencyModel");
+const coins = require("../acronym.json");
 
 const resourceUrl = process.env.TICKER_API;
 const CurrencyOID = process.env.CURRENCY_OID;
-
 class currencyController {
   async update() {
     try {
       const data = await fetch(resourceUrl);
-      const body = await data.json();
-      const result = await Currency.findByIdAndUpdate(CurrencyOID, body);
-      console.log(result);
+      const results = await data.json();
+      const currency = {};
+      results.forEach((item) => {
+        const coinName = item.name;
+        const acronym = item.symbol.toUpperCase();
+        const change24H = item?.market_data?.price_change_percentage_24h;
+        const change1YR = item?.market_data?.price_change_percentage_1y;
+        const inr_obj = {
+          coinName,
+          price: item?.market_data?.current_price?.inr,
+          change24H,
+          change1YR,
+        };
+        const usd_obj = {
+          coinName,
+          price: item?.market_data?.current_price?.usd,
+          change24H,
+          change1YR,
+        };
+        currency[`${acronym}USDT`] = usd_obj;
+        currency[acronym] = inr_obj;
+      });
+      await Currency.findByIdAndUpdate(CurrencyOID, currency);
     } catch (err) {
       console.log(err);
     }
   }
 
-  async getTop(req,res) {
+  async getTop(req, res) {
     try {
       const id = process.env.CURRENCY_OID;
       const results = await Currency.findById(id);
-      const {BTC,BNB,ETH,DOGE,ADA,XRP} = results._doc;
+      const { BTCUSDT, BNBUSDT, ETHUSDT, DOGEUSDT, ADAUSDT, XRPUSDT } =
+        results._doc;
       // console.log(results._doc.BTC);
-      const filteredResults = {BTC,BNB,ETH,DOGE,ADA,XRP};
+      const filteredResults = {
+        BTCUSDT,
+        BNBUSDT,
+        ETHUSDT,
+        DOGEUSDT,
+        ADAUSDT,
+        XRPUSDT,
+      };
       const resultArray = [];
       for (const [key, value] of Object.entries(filteredResults)) {
-        const name = key.includes('USDT') ? key.replace(/USDT/g, '') : key.toLowerCase();
-        const coinName = coins[name.toLowerCase()];
-        resultArray.push({
-          coinName,
-          acronym: key,
-          highest_buy_bid: value && value.highest_buy_bid,
-          lowest_sell_bid: value && value.lowest_sell_bid,
-          last_traded_price: value && value.last_traded_price,
-          yes_price: value && value.yes_price,
-          volumeMax: value && value.volume && value.volume.max,
-          volumeMin: value && value.volume && value.volume.min,
-          volume: value && value.volume && value.volume.volume,
-        });
+        const obj = {acronym: key,...value};
+        resultArray.push(obj);
       }
-      res.send({status: 1, msg: 'Data fetched Successfully', data: resultArray });
+      res.send({
+        status: 1,
+        msg: "Data fetched Successfully",
+        data: resultArray,
+      });
     } catch (err) {
       console.log(err);
-      res.status(500).send({status: 0, msg: 'Internal Server Error'});
+      res.status(500).send({ status: 0, msg: "Internal Server Error" });
     }
   }
 
-  async getAll(req,res) {
+  async getAll(req, res) {
     try {
       const id = process.env.CURRENCY_OID;
       const results = await Currency.findById(id);
-      const { _id, ...final} = results._doc;
+      // eslint-disable-next-line no-unused-vars
+      const { _id, __v, ...filteredData } = results._doc;
       const resultArray = [];
-      for (const [key, value] of Object.entries(final)) {
-        const name = key.includes('USDT') ? key.replace(/USDT/g, '') : key.toLowerCase();
-        const coinName = coins[name.toLowerCase()];
-        resultArray.push({
-          coinName,
-          acronym: key,
-          highest_buy_bid: value && value.highest_buy_bid,
-          lowest_sell_bid: value && value.lowest_sell_bid,
-          last_traded_price: value && value.last_traded_price,
-          yes_price: value && value.yes_price,
-          volumeMax: value && value.volume && value.volume.max,
-          volumeMin: value && value.volume && value.volume.min,
-          volume: value && value.volume && value.volume.volume,
-        });
+      for (const [key, value] of Object.entries(filteredData)) {
+        const obj = {acronym: key,...value};
+        resultArray.push(obj);
       }
-      res.send({status: 1, msg: 'Data fetched Successfully', data: resultArray });
+      res.send({
+        status: 1,
+        msg: "Data fetched Successfully",
+        data: resultArray
+      });
     } catch (err) {
       console.log(err);
-      res.status(500).send({status: 0, msg: 'Internal Server Error'});
+      res.status(500).send({ status: 0, msg: "Internal Server Error" });
     }
   }
 }
