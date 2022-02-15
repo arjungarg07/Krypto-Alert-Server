4e6815b7690e7c4f2e1c2ad04a78fc491e670924
arjungarg07 <arjun.garg.cse01@gmail.com>
2022-02-15 21:08:06 +0530

    chore: cron for every 3 hours

--- jobs/updateCurrency.js
+++ jobs/updateCurrency.js
@@ -2,6 +2,6 @@ const CronJob = require('cron').CronJob;
 
 const currencyController = require('../controllers/currencyController');
 
-module.exports = new CronJob('0 */6 * * *', (async() => {
+module.exports = new CronJob('0 */3 * * *', (async() => {
   await currencyController.update();
 }), null, false, 'America/Los_Angeles');
